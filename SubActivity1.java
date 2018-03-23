b44a2887f17463e699ae84ca512e2ee9a938dddf
kwackjihoon <chunjoo4@naver.com>
2018-03-23 20:37:06 +0900

    Add Third view

--- SubActivity1.java
+++ SubActivity1.java
@@ -1,8 +1,11 @@
 package com.example.sec.prjchangeactivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by sec on 2018-03-22.
@@ -13,5 +16,22 @@ public class SubActivity1 extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+
+        Button btFirst = findViewById(R.id.btFirst);
+        btFirst.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btThird = findViewById(R.id.btThird);
+        btThird.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SubActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
